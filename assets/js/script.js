03b15073706633af85000497377c0c7e1738718b
jack <biffisbuffm@gmail.com>
2020-07-10 12:50:26 -0500

    edited api call order'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,28 @@
 submitBtn = document.getElementById("submitBtn");
 
-//
-
 // function that captures input
 var submitZip = function(event) {
     // capture text entered for city
     var zipCode = $('#zipCodeEntry').val().trim();
-    // send city to api call
-
-    // if city and state entered, clear field
+    // if zip code entered, clear field
     if (zipCode){
-        
         // clear entry field
         zipCodeEntry.value = "";
-
         // if entry field is not a number
     } if (isNaN(zipCode)) {
-        
         // prompt user to enter zip code
         zipCodeEntry.value = "Please enter a zipcode";
-    } 
+    } ;
 
+    console.log(zipCode);
+    // send zipcode' captured value to first Api call
+    getJobs();
 };
 
-// function to use first api
-var getWageInfo = function(state) {
-    // establish URL
-    wageDataUrl = "https://research.zippia.com/living-wage.html";
-    
-    // pass URL to fetch
-    fetch(wageDataUrl)
-    // handling the data
-    .then(function(){
-        // return the text value as an integer
-
-    
+// function for first api call
 
 
-    })
-    
-};
 //  when user clicks on the div, it links to the job posting on indeed
-
-
-
 
 // when user clicks submit btn, runs function
 submitBtn.addEventListener("click", submitZip);
