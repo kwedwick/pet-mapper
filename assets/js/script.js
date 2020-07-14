c0e3d0a66e0a5cec32918ab887bee94e99b2113d
londonlast21 <londonlast21@gmail.com>
2020-07-14 16:35:20 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,7 @@
 
-
 // function to generate map pins
 var sendToMap = function (){
     console.log("hello");
-}
-
-// //Adding TomTom Stuff
-
-// // Define your product name and version.
-// tt.setProductInfo('Codepen Examples', '${analytics.productVersion}');
-// var map = tt.map({
-//     key: 'ejoYhQhApDJfoTII6fG63l3BXF0tiaUV',
-//     container: 'map',
-//     style: 'tomtom://vector/1/basic-main',
-//     dragPan: !isMobileOrTablet(),
-//     center: [-99.98580752275456, 33.43211082128627],
-//     zoom: 3
-// });
-// map.addControl(new tt.FullscreenControl());
-// map.addControl(new tt.NavigationControl());
-// function createMarker(icon, position, color, popupText) {
-//     var markerElement = document.createElement('div');
-//     markerElement.className = 'marker';
-//     var markerContentElement = document.createElement('div');
-//     markerContentElement.className = 'marker-content';
-//     markerContentElement.style.backgroundColor = color;
-//     markerElement.appendChild(markerContentElement);
-//     var iconElement = document.createElement('div');
-//     iconElement.className = 'marker-icon';
-//     iconElement.style.backgroundImage =
-//         'url(https://api.tomtom.com/maps-sdk-for-web/5.x/assets/images/' + icon + ')';
-//     markerContentElement.appendChild(iconElement);
-//     var popup = new tt.Popup({offset: 30}).setText(popupText);
-//     // add marker to map
-//     new tt.Marker({element: markerElement, anchor: 'bottom'})
-//         .setLngLat(position)
-//         .setPopup(popup)
-//         .addTo(map);
-// }
-// createMarker('accident.colors-white.svg', [-120.72217631449985, 42.73919549715691], '#5327c3', 'SVG icon');
-// createMarker('accident.colors-white.png', [-99.98580752275456, 33.43211082128627], '#c30b82', 'PNG icon');
-// createMarker('accident.colors-white.jpg', [43.0731, 89.4012], '#c31a26', 'JPG icon');
-
-
-// function to send api call to petfinder
-var findPets = function() {
-    // petfinder api call
-    $.get()
-    .then(function(response) {
-        //function that generates pins
-        sendtoMap();
-    })
-
-
 }
 
 // capture entered city and state and animal type
@@ -66,7 +15,7 @@ var getLocation = function () {
         console.log(city);
         cityEntry.value = "";
 
-        // if entry field is left blank or if the city 
+    //     // if entry field is left blank or if the city 
     } else {
         //need another option; she said no alerts
         alert("Please choose a city for more specific results");
@@ -74,9 +23,30 @@ var getLocation = function () {
 
     // checking to see captured values
     console.log(city, state, pet);
-
-    // send captured values to petfinder
-    findPets();
+    
+      var pf = new petfinder.Client({
+        apiKey: "d9CrIalA9BqDadPoKDdacOdlOsPFm6UDYC00zRok4S5duTHiTQ",
+        secret: "tR9LMFUA157fS4G2xAJYqCbGPjiGM7tw0Qi15sIc",
+      });
+      
+      var apiKey = "d9CrIalA9BqDadPoKDdacOdlOsPFm6UDYC00zRok4S5duTHiTQ";
+      var type = pet;
+      var petLocation = city, state;
+      
+      pf.animal
+        .search({
+          type: pet,
+          //location: "11111",
+          //limit: 100,
+        })
+        .then(function (response) {
+          // Do something with `response.data.animals`
+          console.log(response);
+        })
+        .catch(function (error) {
+          // Handle the error
+        });
+      ;
 
 }
 
